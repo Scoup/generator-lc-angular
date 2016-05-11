cbf9ab091f6a40cd965c79059265ee5d1ae66146
Léo Haddad Carneiro <scoup001@gmail.com>
2016-05-11 15:27:48 -0300

    Changed partial to use yeoman extend

--- partial/index.js
+++ partial/index.js
@@ -1,58 +1,54 @@
 'use strict';
-var util = require('util');
 var yeoman = require('yeoman-generator');
-var path = require('path');
-var fs = require('fs');
 var cgUtils = require('../utils.js');
 var _ = require('underscore');
-var chalk = require('chalk');
-var fs = require('fs');
 var url = require('url');
 
-_.str = require('underscore.string');
-_.mixin(_.str.exports());
+module.exports = yeoman.Base.extend({
+    constructor: function() {
+        yeoman.Base.apply(this, arguments);
+    },
 
-var PartialGenerator = module.exports = function PartialGenerator(args, options, config) {
+    askFor: function() {
+        var cb = this.async();
 
-    cgUtils.getNameArg(this,args);
+        var prompts = [
+            {
+                name: 'route',
+                message: 'Enter your route url (i.e. /mypartial/:id).  If you don\'t want a route added for you, leave this empty.'
+            }
+        ];
 
-    yeoman.generators.Base.apply(this, arguments);
+        cgUtils.addNamePrompt(this,prompts,'partial');
 
-};
+        this.prompt(prompts, function (props) {
+            if (props.name){
+                this.name = props.name;
+            }
+            this.route = url.resolve('',props.route);
+            cgUtils.askForModuleAndDir('partial',this,true,cb);
+        }.bind(this));
+    },
 
-util.inherits(PartialGenerator, yeoman.generators.Base);
+    files: function() {
+        this.ctrlname = _.camelize(_.classify(this.name)) + 'Ctrl';
+        this.uirouter = this.config.get('uirouter');
 
-PartialGenerator.prototype.askFor = function askFor() {
-    var cb = this.async();
+        cgUtils.processTemplates(this.name,this.dir,'partial',this,null,null,this.module);
 
-    var prompts = [
-        {
-            name: 'route',
-            message: 'Enter your route url (i.e. /mypartial/:id).  If you don\'t want a route added for you, leave this empty.'
+        if (this.route && this.route.length > 0){
+            var partialUrl = this.dir + this.name + '.html';
+            cgUtils.injectRoute(this.module.file,this.config.get('uirouter'),this.name,this.route,partialUrl,this);
         }
-    ];
+    },
 
-    cgUtils.addNamePrompt(this,prompts,'partial');
-
-    this.prompt(prompts, function (props) {
-        if (props.name){
-            this.name = props.name;
-        }
-        this.route = url.resolve('',props.route);
-        cgUtils.askForModuleAndDir('partial',this,true,cb);
-    }.bind(this));
-};
-
-PartialGenerator.prototype.files = function files() {
-
-    this.ctrlname = _.camelize(_.classify(this.name)) + 'Ctrl';
-    this.uirouter = this.config.get('uirouter');
-
-    cgUtils.processTemplates(this.name,this.dir,'partial',this,null,null,this.module);
-
-    if (this.route && this.route.length > 0){
-        var partialUrl = this.dir + this.name + '.html';
-        cgUtils.injectRoute(this.module.file,this.config.get('uirouter'),this.name,this.route,partialUrl,this);
+    _generateUiRoute: function(controller,route,routeUrl) {
+        routeUrl = routeUrl.replace(/\\/g,'/');
+        var options = {
+            url: route,
+            templateUrl: routeUrl,
+            controller: controller
+        };
+        return '$routeProvider.when(' + route + ',' + options.toString() + ');';
     }
-
-};
+});
